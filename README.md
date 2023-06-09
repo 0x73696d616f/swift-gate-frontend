d8d7442c08d2beed4dc74ea0ba8c69ddf44a203a
Carolina-Pinheiro <57190987+Carolina-Pinheiro@users.noreply.github.com>
2023-06-09 13:00:19 +0200

    feat: frontend homepage

--- README.md
+++ README.md
@@ -1,34 +1,31 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Next.js + Ethers.js starter template
+
+A boilerplate template for your Next.js and Ethers.js dapp.
+
+## Built with
+
+| Framework                                         | Description                                             |
+| ------------------------------------------------- | ------------------------------------------------------- |
+| [Ethers.js](https://www.npmjs.com/package/ethers) | A library for interacting with the Ethereum blockchain. |
+| [Next.js](https://www.npmjs.com/package/next)     | A framework for building fast and responsive web apps.  |
 
 ## Getting Started
 
-First, run the development server:
+- Clone the repository
+- Install dependencies `npm install`
+- Run `npm run dev` to start the development server
+- Open the app in a browser
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-```
+### Screenshots
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+#### 🟢 Connected
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+<img src="screenshots/connected.png" alt="Connected to metamask" width="500">
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+#### Default status
 
-## Learn More
+<img src="screenshots/not-connected.png" alt="Default status" width="500">
 
-To learn more about Next.js, take a look at the following resources:
+#### No Metamask
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+<img src="screenshots/no-metamask.png" alt="User dont have metamask installed" width="500">
---
+++ pages/index.js
@@ -0,0 +1,200 @@
+import { useState, useEffect } from "react";
+import { dropdownOptions } from "../utils/dropdownOptions";
+import { Dropdown } from "@nextui-org/react";
+import React from "react";
+import { Bridge1Icon } from "../utils/bridge1icon";
+import { Bridge2Icon } from "../utils/bridge2icon";
+import { Bridge3Icon } from "../utils/bridge3icon";
+
+import NextLink from "next/link";
+import Link from "next/link";
+import Metamask from "../component/metamask";
+
+const Index = () => {
+  const [haveMetamask, sethaveMetamask] = useState(true);
+  
+  const [client, setclient] = useState({
+    isConnected: false,
+  });
+
+  const [selectedOrigin, setSelectedOrigin] = React.useState(new Set(["Origin Bridge"]));
+  const [selectedDestination, setSelectedDestination] = React.useState(new Set(["Destination Bridge"]));
+
+  const selectedValueOrigin = React.useMemo(
+    () => Array.from(selectedOrigin).join(", ").replaceAll("_", " "),
+    [selectedOrigin]
+  );
+
+  const selectedValueDestination = React.useMemo(
+    () => Array.from(selectedDestination).join(", ").replaceAll("_", " "),
+    [selectedDestination]
+  );
+
+  const checkConnection = async () => {
+    const { ethereum } = window;
+    if (ethereum) {
+      sethaveMetamask(true);
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      if (accounts.length > 0) {
+        setclient({
+          isConnected: true,
+          address: accounts[0],
+        });
+      } else {
+        setclient({
+          isConnected: false,
+        });
+      }
+    } else {
+      sethaveMetamask(false);
+    }
+  };
+
+  const connectWeb3 = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Metamask not detected");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setclient({
+        isConnected: true,
+        address: accounts[0],
+      });
+    } catch (error) {
+      console.log("Error connecting to metamask", error);
+    }
+  };
+
+
+  useEffect(() => {
+    checkConnection();
+  }, []);
+
+  return (
+    <>
+      {/* Navbar */}
+      <nav className="fren-nav d-flex">
+        <div className="d-flex" style={{ marginLeft: "auto" }}>
+          <div>
+            <button className="btn connect-btn" onClick={connectWeb3}>
+              {client.isConnected ? (
+                <>
+                  {client.address.slice(0, 4)}...
+                  {client.address.slice(38, 42)}
+                </>
+              ) : (
+                <>Connect Wallet</>
+              )}
+            </button>
+          </div>
+          <div>
+            <Link href="https://twitter.com/threesigma_xyz">
+              <button className="btn tw-btn">TW</button>
+            </Link>
+          </div>
+        </div>
+      </nav>
+      {/* Navbar end */}
+
+      <section className="container d-flex">
+        <main>
+          <h1 className="main-title">SwiftGate ⛩️</h1>
+
+          <p className="main-desc">
+            SwiftGate is a bridging protocol that allows users to bridge their assets between different chains with minimal costs. 
+             <br />
+          </p>
+
+          {/* ---- */}
+          <p>
+            {!haveMetamask ? (
+              <Metamask />
+            ) : client.isConnected ? (
+              <>
+                <br />
+                <h2>You're connected ✅</h2>
+                <div className="bridge-dropdowns-container" style={{ marginTop: "20px" }}>
+                  <div className="bridge-dropdowns">
+                    <Dropdown>
+                      <Dropdown.Button auto color="secondary">{selectedValueOrigin}</Dropdown.Button>
+                      <Dropdown.Menu 
+                        aria-label="OriginActions"
+                        selectionMode="single"
+                        selectedKeys={selectedOrigin}
+                        onSelectionChange={setSelectedOrigin}>
+                        <Dropdown.Item 
+                          key="Origin: bridge 1"
+                          icon={<Bridge1Icon size={22} fill="var(--nextui-colors-secondary)" />}
+                         ><NextLink href="/">bridge 1</NextLink></Dropdown.Item>
+                        <Dropdown.Item 
+                          key="Origin: bridge 2"
+                          icon={<Bridge2Icon size={22} fill="var(--nextui-colors-secondary)" />}
+                          ><NextLink href="/">bridge 2</NextLink></Dropdown.Item>
+                        <Dropdown.Item 
+                          key="Origin: bridge 3"
+                          icon={<Bridge3Icon size={22} fill="var(--nextui-colors-secondary)" />}
+                          ><NextLink href="/">bridge 3</NextLink></Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+
+                    <Dropdown>
+                      <Dropdown.Button auto color="secondary">{selectedValueDestination}</Dropdown.Button>
+                      <Dropdown.Menu 
+                        aria-label="DestinationActions"
+                        selectionMode="single"
+                        selectedKeys={selectedDestination}
+                        onSelectionChange={setSelectedDestination}>
+                        <Dropdown.Item 
+                          key="Origin: bridge 1"
+                          icon={<Bridge1Icon size={22} fill="var(--nextui-colors-secondary)" />}
+                         ><NextLink href="/">bridge 1</NextLink></Dropdown.Item>
+                        <Dropdown.Item 
+                          key="Origin: bridge 2"
+                          icon={<Bridge2Icon size={22} fill="var(--nextui-colors-secondary)" />}
+                          ><NextLink href="/">bridge 2</NextLink></Dropdown.Item>
+                        <Dropdown.Item 
+                          key="Origin: bridge 3"
+                          icon={<Bridge3Icon size={22} fill="var(--nextui-colors-secondary)" />}
+                          ><NextLink href="/">bridge 3</NextLink></Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <br />
+                <button className="btn connect-btn" onClick={connectWeb3}>
+                  Connect Wallet
+                </button>
+              </>
+            )}
+          </p>
+          {/* ---- */}
+        </main>
+      </section>
+
+      <style jsx>{`
+        .bridge-dropdowns-container {
+          display: flex;
+          justify-content: center;
+          gap: 10px;
+        }
+        
+        .bridge-dropdowns {
+          display: flex;
+          gap: 10px;
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default Index;
